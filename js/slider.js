c497ea04896b79a7b754247fafefabb440ace89b
ramesh <ramesh334u@gmail.com>
2015-06-12 09:07:27 +0530

    Gallery changes

---
+++ js/slider.js
@@ -0,0 +1,65 @@
+var app = angular.module('app', []);
+app.controller('sliderCtrl', ['$scope', '$http', function($scope, $http){
+	$scope.posts = [];
+	$scope.currentImagePath = "";
+	$scope.caption = "";
+	$scope.description = "";
+	$scope.currentIndex = 0;
+	$scope.count = 0;
+
+	$http.get("data.json").success(function(response) {
+    	$scope.posts = response;
+    	$scope.count = response.length;
+    	$scope.bindImage($scope.currentIndex);    	
+    });
+
+    $scope.bindImage = function(index){
+    	if($scope.posts[index]){
+	    	$scope.currentImagePath = $scope.posts[index].image;
+	    	$scope.caption = $scope.posts[index].headline;
+			$scope.description = $scope.posts[index].body;
+    	}
+    };
+
+    $scope.prev = function(){
+		$scope.currentIndex--;
+    };
+
+    $scope.next = function(){
+		$scope.currentIndex++;
+    };
+
+    $scope.handleLeftRightKeys = function ($event) {
+    	if($event.keyCode === 37) { 
+    		$scope.currentIndex-- ;
+		} 
+		else if($event.keyCode === 39) {
+			$scope.currentIndex++; 
+		}
+    };
+
+    $scope.$watch(function(){ return $scope.currentIndex;}, function(current, prev){
+    	if(current < 0){
+    		$scope.currentIndex = $scope.count + current;    		
+    	}
+    	else if(current > ($scope.count-1)){
+    		$scope.currentIndex = current - $scope.count;
+    	}
+    	else{
+    		$scope.bindImage(current);
+    	}
+    });    
+
+}]);
+
+app.directive('fadeIn', function($timeout){
+    return {
+        restrict: 'A',
+        link: function($scope, $element, attrs){
+            $element.addClass("ng-hide-remove");
+            $element.on('load', function() {
+                $element.addClass("ng-hide-add");
+            });
+        }
+    }
+});
