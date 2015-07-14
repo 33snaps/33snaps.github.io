c9e5b6d4a841619cf34eaee0d717cddee7323732
ramesh533 <ramesh0533@gmail.com>
2015-07-14 15:36:10 +0530

    Update slider.js

--- js/slider.js
+++ js/slider.js
@@ -1,4 +1,4 @@
-var app = angular.module('app', []);
+var app = angular.module('app', ['ngSanitize']);
 app.controller('sliderCtrl', ['$scope', '$http', function($scope, $http){
 	$scope.posts = [];
 	$scope.currentImagePath = "";
